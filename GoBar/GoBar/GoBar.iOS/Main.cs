a9bb30be4ff063ce12fd54055ea5bc16d85f1620
JRamirezRGomez <ramirez.gomez1995@gmail.com>
2018-06-26 19:26:09 -0500

    Ejemplo

--- GoBar/GoBar/GoBar.iOS/Main.cs
+++ GoBar/GoBar/GoBar.iOS/Main.cs
@@ -12,6 +12,7 @@ namespace GoBar.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            //Go Fuk Yourself
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
